7931b8b1e78b5b9ebcc484f9cc47723e8a2ce523
toproad <tattun0817@gmail.com>
2023-06-15 14:51:00 +0900

    imgタグの書き換え

--- src/app/components/topChefCard.tsx
+++ src/app/components/topChefCard.tsx
@@ -1,6 +1,7 @@
 "use client"
 import "swiper/swiper.min.css"
 
+import Image from "next/image"
 import { FC } from "react"
 import React from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
@@ -12,11 +13,16 @@ type CardProps = {
 
 const Card: FC<CardProps> = ({ image, title }) => (
   <div className=" relative overflow-hidden">
-    <div style={{ paddingTop: "148%" }}></div>
-    <img
+    <div
+      className="relative w-full h-full"
+      style={{ paddingTop: "148%" }}
+    ></div>
+    <Image
       src={image}
       alt=""
-      className="absolute top-0 left-0 w-full h-full object-cover rounded-3xl"
+      width={148}
+      height={220}
+      className="absolute top-0 left-0 w-full h-full rounded-3xl"
     />
     <h2 className="absolute bottom-2.5 left-0 w-full text-white text-base truncate text-center">
       {title}
