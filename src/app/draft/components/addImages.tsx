64aac6fda6173a0ca8cef4ff7a767c64db75d7be
jungle379 <jungle379@gmail.com>
2023-06-16 12:26:36 +0900

    ⚡️ 修正

--- src/app/draft/components/addImages.tsx
+++ src/app/draft/components/addImages.tsx
@@ -1,17 +1,16 @@
-"use client"
-
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 
 export const AddImages = () => {
-  const [image, setImage] = useState("")
+  const [image, setImage] = useState<string | null>(null)
 
-  const handleImageAdd = () => {
-    // 画像追加の処理
-    // 例えば、ファイルの選択ダイアログを表示して画像を選択し、その画像を表示するような処理を実装します
-    // ここでは仮の実装として、固定の画像パスを使用しています
-    const imagePath = "path/to/image.jpg"
-    setImage(imagePath)
+  const handleImageAdd = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0] // 選択されたファイルを取得
+    if (file) {
+      const imageURL = URL.createObjectURL(file) // ファイルのURLを生成
+      setImage(imageURL)
+    }
   }
+
   return (
     <>
       <div className="h-[19px] mt-[32px] mx-[16px] font-bold text-[16px]">
@@ -21,20 +20,32 @@ export const AddImages = () => {
         <div className="flex">
           {image ? (
             // eslint-disable-next-line @next/next/no-img-element
-            <img src={image} alt="" className="h-full" />
+            <img src={image} alt="新しい画像" className="h-full" />
           ) : (
-            <div>※画像入れ込む※</div>
+            <div></div>
           )}
         </div>
       </div>
       <div className="h-[27px] flex text-red-400">
-        <div className="ml-4 mr-1 mt-[9px] h-4 w-4" onClick={handleImageAdd}>
-          +
+        <div className="ml-4 mr-1 mt-[9px] h-4 w-4">
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageAdd}
+            style={{ display: "none" }}
+            id="image-upload-input"
+          />
+          <label htmlFor="image-upload-input">+</label>
         </div>
         <div className="h-[19px] w-[112px] text-[16px] mt-[8px]">
-          <a href="#" onClick={handleImageAdd}>
-            画像を追加する
-          </a>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageAdd}
+            style={{ display: "none" }}
+            id="image-upload-input"
+          />
+          <label htmlFor="image-upload-input">画像を追加する</label>
         </div>
       </div>
     </>
