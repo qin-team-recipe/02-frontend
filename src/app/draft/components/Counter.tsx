342c1f2271688bd1fa0af734e850146d6a48b249
jungle379 <jungle379@gmail.com>
2023-06-20 11:44:19 +0900

    *人前のカウンター修正

--- src/app/draft/components/Counter.tsx
+++ src/app/draft/components/Counter.tsx
@@ -6,7 +6,9 @@ export const Counter = () => {
   const [count, setCount] = useState(1)
 
   const increment = () => {
-    setCount(count + 1)
+    if (count < 6) {
+      setCount(count + 1)
+    }
   }
 
   const decrement = () => {
@@ -18,16 +20,23 @@ export const Counter = () => {
   return (
     <>
       <div className="h-[19px] px-[16px] flex mb-1">
-        <div className="font-bold text-[16px] mr-[6.67px]">
+        <div className="font-bold text-[16px] w-24 mr-[6.67px]">
           材料/{count}人前
         </div>
         <button
-          className={`h-[16px] ${count === 1 ? "text-gray-400" : ""}`}
+          className={`text-[16px] h-5 w-5 flex justify-center item-center ${
+            count === 1 ? "text-gray-400" : "text-red-500 bg-red-200"
+          }`}
           onClick={decrement}
         >
           -
         </button>
-        <button className="h-[16px]" onClick={increment}>
+        <button
+          className={`ml-2 text-[16px] h-5 w-5 flex justify-center item-center ${
+            count === 6 ? "text-gray-400" : "text-red-500 bg-red-200"
+          }`}
+          onClick={increment}
+        >
           +
         </button>
       </div>
