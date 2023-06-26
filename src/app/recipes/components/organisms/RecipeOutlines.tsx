02ca4cd42adb10f284e3c9a2a948395f2b3c7771
daisuketujita <daisuketujita@gmail.com>
2023-06-27 08:23:03 +0900

    お気に入り登録は、ハート型ボタンではなく「お気に入りに追加する」の文字のボタンにする

--- src/app/recipes/components/organisms/RecipeOutlines.tsx
+++ src/app/recipes/components/organisms/RecipeOutlines.tsx
@@ -41,7 +41,7 @@ const getRecipeOutlineData = async (id: string): Promise<RecipeOutlineType> => {
       "はじめてでも失敗なく作れるような、鶏肉や玉ねぎを具とした基本的なマカロニグラタンのレシピです。\n" +
       "ソースと具材炒めを別器具で行うレシピも多いですが、グラタンの具を炒めたフライパンの中で、そのままホワイトソースを仕上げる手軽な作り方にしています。ぜひお試しください。",
     favoriteCount: 768,
-    imageUrl: "https://placehold.jp/400x400.png",
+    imageUrl: "https://placehold.jp/400x300.png",
     isMyFavorite: false,
   }
 }
@@ -56,22 +56,16 @@ const RecipeOutlines = async (props: RecipeOutlinesProps) => {
 
   return (
     <>
-      <div className="w-full relative">
+      <div className="w-full">
         {/* レシピ画像 */}
         <ImageWithBlurType src={recipe.imageUrl} alt={recipe.title} />
-
-        {/* お気に入りボタン */}
-        <RecipeFavoriteButton
-          className="absolute right-0 bottom-1"
-          isMyFavorite={recipe.isMyFavorite}
-        />
       </div>
       <div className="p-4">
         {/* レシピタイトル */}
-        <div className="font-bold mb-2 text-2xl">{recipe.title}</div>
+        <div className="mb-2 text-2xl font-bold">{recipe.title}</div>
 
         {/* レシピ説明 */}
-        <div className="h-full mb-4">{recipe.description}</div>
+        <div className="mb-4 h-full">{recipe.description}</div>
 
         {/* シェフ */}
         <div className="flex flex-row items-center">
@@ -83,6 +77,14 @@ const RecipeOutlines = async (props: RecipeOutlinesProps) => {
 
           {/* お気に入り件数 */}
           <FavoriteCounterLabel className="ml-4" count={recipe.favoriteCount} />
+        </div>
+
+        {/* お気に入りボタン */}
+        <div>
+          <RecipeFavoriteButton
+            className="mt-2"
+            isMyFavorite={recipe.isMyFavorite}
+          />
         </div>
       </div>
     </>
