e984aa5f63f25e5a10030ef3797376b079fd4955
toproad <tattun0817@gmail.com>
2023-07-20 16:28:25 +0900

    コンポーネントの値修正

--- src/app/components/container.tsx
+++ src/app/components/container.tsx
@@ -6,7 +6,7 @@ type ContainerProps = {
 
 const Container: FC<ContainerProps> = ({ children }) => {
   return (
-    <div className="-ml-[2px] -mr-[2px]  border-l-2 border-r-2">
+    <div className="-ml-[2px] -mr-[2px] ">
       <div className=" mx-auto mb-[90px] w-full max-w-[480px] flex-grow px-4 py-4 md:mx-0 md:mb-0 ">
         {children}
       </div>
