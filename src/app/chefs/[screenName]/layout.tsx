f520699a1b87ac6e47e3012d5f13570d23333663
daisuketujita <daisuketujita@gmail.com>
2023-07-25 09:02:56 +0900

    ・ページバックボタンのレイアウトずれを修正

--- src/app/chefs/[screenName]/layout.tsx
+++ src/app/chefs/[screenName]/layout.tsx
@@ -10,8 +10,7 @@ export const metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* TODO 全体レイアウト決定前のため暫定的にここでレスポンシブ対応 */}
-      <div className="mx-auto flex min-h-screen sm:max-w-2xl">
+      <div className="relative">
         <div className="absolute left-4 top-4 z-10 text-black">
           <PageBackButton />
         </div>
