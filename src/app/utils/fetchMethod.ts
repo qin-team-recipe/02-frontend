43045dda0be12380845e5ade575d864a74a0925a
toproad <tattun0817@gmail.com>
2023-07-25 23:28:12 +0900

    トークンあり用Fetchメソッドの実装

--- src/app/utils/fetchMethod.ts
+++ src/app/utils/fetchMethod.ts
@@ -1,3 +1,5 @@
+import { getTokenFromLocalStorage } from "./localStorage"
+
 type FetchOptions = {
   url: string
   method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH"
@@ -10,6 +12,32 @@ export const fetchData = async ({ url, method, body }: FetchOptions) => {
     method,
     headers: {
       "Content-Type": "application/json",
+    },
+  }
+
+  if (body) {
+    options.body = JSON.stringify(body)
+  }
+
+  const response = await fetch(`${API_URL}${url}`, options)
+
+  if (response.ok) {
+    const data = await response.json()
+    return data
+  } else {
+    throw new Error("Error: " + response.status)
+  }
+}
+
+export const fetchAuthData = async ({ url, method, body }: FetchOptions) => {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL
+  const token = getTokenFromLocalStorage() // get token here
+  const options: any = {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      // Bearerが必要かは要確認
+      ...(token && { Authorization: `Bearer ${token}` }),
     },
   }
 
