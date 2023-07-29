2a74c2794dfefeb248911ad353aeed3930b77af9
toproad <tattun0817@gmail.com>
2023-07-29 11:44:46 +0900

    fetchメソッドのオプション修正

--- src/app/utils/fetchMethod.ts
+++ src/app/utils/fetchMethod.ts
@@ -1,18 +1,31 @@
+"use client"
 import { getTokenFromLocalStorage } from "./localStorage"
 
 type FetchOptions = {
   url: string
   method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH"
+  cache?: "no-cache" | "reload" | "force-cache" | "only-if-cached" | "no-store"
+  contentType?: string
+  next?: object
   body?: any
 }
 
-export const fetchData = async ({ url, method, body }: FetchOptions) => {
+export const fetchData = async ({
+  url,
+  method,
+  cache,
+  next,
+  contentType = "application/json",
+  body,
+}: FetchOptions) => {
   const API_URL = process.env.NEXT_PUBLIC_API_URL
   const options: any = {
-    method,
+    method: method,
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": contentType,
     },
+    cache: cache,
+    next: next,
   }
 
   if (body) {
@@ -29,16 +42,25 @@ export const fetchData = async ({ url, method, body }: FetchOptions) => {
   }
 }
 
-export const fetchAuthData = async ({ url, method, body }: FetchOptions) => {
+export const fetchAuthData = async ({
+  url,
+  method,
+  cache,
+  next,
+  contentType = "application/json",
+  body,
+}: FetchOptions) => {
   const API_URL = process.env.NEXT_PUBLIC_API_URL
   const token = getTokenFromLocalStorage() // get token here
   const options: any = {
-    method,
+    method: method,
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": contentType,
       // Bearerが必要かは要確認
       ...(token && { Authorization: `Bearer ${token}` }),
     },
+    cache: cache,
+    next: next,
   }
 
   if (body) {
