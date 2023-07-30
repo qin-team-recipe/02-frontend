228c4aef57e0d8ccef54511433a89f6b2d6f1a76
toproad <tattun0817@gmail.com>
2023-07-30 19:49:41 +0900

    トップページからシェフ一覧のフェッチ

--- src/app/components/topChefPicCard.tsx
+++ src/app/components/topChefPicCard.tsx
@@ -2,31 +2,35 @@
 import "swiper/swiper.min.css"
 
 import Image from "next/image"
+import Link from "next/link"
 import { FC } from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
 
-import { ChefCardProps } from "../types"
-const Card: FC<ChefCardProps> = ({ image, title }) => (
-  <div className=" relative overflow-hidden">
-    <div
-      className="relative h-full w-full"
-      style={{ paddingTop: "148%" }}
-    ></div>
-    <Image
-      src={image}
-      alt=""
-      width={148}
-      height={220}
-      className="absolute left-0 top-0 h-full w-full rounded-3xl"
-    />
-    <h2 className="absolute bottom-2.5 left-0 w-full truncate text-center text-base text-white">
-      {title}
-    </h2>
-  </div>
+import { ChefProps } from "../types"
+
+const Card: FC<ChefProps> = ({ imageUrl, display_name, screen_name }) => (
+  <Link href={`/chefs/${screen_name}`}>
+    <div className="relative block overflow-hidden">
+      <div
+        className="relative h-full w-full"
+        style={{ paddingTop: "148%" }}
+      ></div>
+      <Image
+        src={imageUrl ? imageUrl : "/toppage/topchef1.jpg"}
+        alt=""
+        width={148}
+        height={220}
+        className="absolute left-0 top-0 h-full w-full rounded-3xl"
+      />
+      <h2 className="absolute bottom-2.5 left-0 w-full truncate text-center text-base text-white">
+        {display_name}
+      </h2>
+    </div>
+  </Link>
 )
 
 type SwiperComponentProps = {
-  data: ChefCardProps[]
+  data: ChefProps[]
 }
 
 const TopChefPicCard: FC<SwiperComponentProps> = ({ data }) => {
--- src/app/types.ts
+++ src/app/types.ts
@@ -1,11 +1,22 @@
-export type ChefCardProps = {
-  image: string
-  title: string
-  firstName?: string
-  lastName?: string
-  recommend?: string
-  recipeCount?: number
+export type ChefProps = {
+  chef_links?: [
+    {
+      chef_id: number
+      id: number
+      service_name: string
+      url: string
+    }
+  ]
+  description: string
+  display_name: string
+  follows_count?: number
+  id: number
+  imageUrl: string
+  is_following?: true
+  recipes_count?: number
+  screen_name: string
 }
+
 export type RecipeCardProps = {
   image: string
   title: string
