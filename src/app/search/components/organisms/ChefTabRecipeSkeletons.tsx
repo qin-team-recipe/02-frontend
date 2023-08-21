04bfcff8078bcf645b200b062700a68a01bd69fd
toproad <tattun0817@gmail.com>
2023-08-21 10:02:48 +0900

    検索ページと話題のレシピの繋ぎこみ

---
+++ src/app/search/components/organisms/ChefTabRecipeSkeletons.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+import Skeleton from "@/app/recipes/commonComponents/atoms/Skeleton"
+
+/**
+ * シェフレシピタブコンテンツスケルトン
+ * @returns
+ */
+const ChefTabRecipeSkeletons = async () => {
+  const recipeImages = [1, 2, 3, 4, 5]
+  return (
+    <>
+      {/* レシピリスト */}
+      <div className=" p-2">
+        <div className="grid grid-cols-2 gap-2">
+          {recipeImages.map((recipeImage, i) => (
+            <div key={i}>
+              <Skeleton className="m-1 h-[170px] w-[170px]"></Skeleton>
+              <Skeleton className="m-1 h-10 w-[170px]"></Skeleton>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+export default ChefTabRecipeSkeletons
