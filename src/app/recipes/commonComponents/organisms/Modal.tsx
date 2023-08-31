66a35d1f862d679316539bdb183a8e686adfdbc3
toproad <tattun0817@gmail.com>
2023-08-31 12:39:08 +0900

    設定画面のログアウトと退会を実装

--- src/app/recipes/commonComponents/organisms/Modal.tsx
+++ src/app/recipes/commonComponents/organisms/Modal.tsx
@@ -3,7 +3,7 @@ import { ReactElement } from "react"
 
 type ModalProps = {
   isOpen: boolean
-  onClose?: () => void
+  onClose?: (value: boolean) => void
   children: ReactElement
 }
 
@@ -11,18 +11,22 @@ const Modal = (props: ModalProps) => {
   const { isOpen, onClose, children } = props
 
   const handleClose = () => {
-    onClose && onClose()
+    onClose && onClose(false)
   }
 
   return (
     <>
       {isOpen && (
         <div className="fixed inset-0 z-20 flex items-center justify-center">
-          <div className="fixed inset-0 bg-black opacity-50"></div>
+          <button
+            className="fixed inset-0 bg-black opacity-50"
+            onClick={handleClose}
+          ></button>
 
           <div className="z-50 rounded-lg bg-white p-4 shadow-lg ">
             {children}
-            {onClose && (
+            {/* 背景画面で閉じることができるので、一旦コメントアウト */}
+            {/* {onClose && (
               <div className=" flex">
                 <button
                   className="ml-auto mt-4 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
@@ -31,7 +35,7 @@ const Modal = (props: ModalProps) => {
                   Close
                 </button>
               </div>
-            )}
+            )} */}
           </div>
         </div>
       )}
