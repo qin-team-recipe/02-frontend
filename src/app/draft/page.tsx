47f17b9c8b576a15c5b7b669a5122d507ca12056
jungle379 <jungle379@gmail.com>
2023-09-12 16:47:50 +0900

    修正

--- src/app/draft/page.tsx
+++ src/app/draft/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { AddImages } from "./components/AddImages"
+import AddIntroduction from "./components/AddIntroduction"
 import { AddLinks } from "./components/AddLinks"
 import { AddMaterial } from "./components/AddMaterial"
 import { AddProcess } from "./components/AddProcess"
@@ -15,14 +16,7 @@ const Draft = () => {
         <AddMaterial />
         <AddProcess />
         <AddImages />
-        <div className="mt-5">
-          <div className="ml-4 h-[19px] text-[16px] font-bold">
-            レシピの紹介文(任意)
-          </div>
-          <div className="mt-1 min-h-[72px] border-y-2">
-            <input className="min-h-[72px] w-full" form="text"></input>
-          </div>
-        </div>
+        <AddIntroduction />
         <AddLinks />
         <FooterButtons />
       </div>
